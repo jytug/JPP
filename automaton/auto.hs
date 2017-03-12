58031624da6649af9234289937e37f671efbf79c
Filip Binkiewicz <filip.binkiewicz@gmail.com>
2017-03-12 20:55:53 +0100

    leftA implemented

--- automaton/auto.hs
+++ automaton/auto.hs
@@ -6,8 +6,6 @@ data Auto a q = A { states      :: [q]
                   , transition  :: q -> a -> [q]
                   }
 
--- symA :: Eq a => a -> Auto a Bool
--- leftA :: Auto a q -> Auto a (Either q r)
 -- sumA :: Auto a q1 -> Auto a q2 -> Auto a (Either q1 q2)
 -- thenA :: Auto a q1 -> Auto a q2 -> Auto a (Either q1 q2)
 -- fromLists :: (Eq q, Eq a) => [q] -> [q] -> [q] -> [(q, a, [a])] -> Auto a q
@@ -22,7 +20,7 @@ acceptsAcc aut (w:ws) states = acceptsAcc aut ws newStates
 accepts :: Eq q => Auto a q -> [a] -> Bool
 accepts aut w = acceptsAcc aut w (initStates aut)
 
--- gives an empty automaton
+-- an empty automaton
 emptyA :: Auto a ()
 emptyA = A { states = []
            , initStates = []
@@ -30,7 +28,7 @@ emptyA = A { states = []
            , transition = curry $ const []
            }
 
--- gives an automaton that recognizes just the empty word
+-- an automaton that recognizes just the empty word
 epsA :: Auto a ()
 epsA = A { states = [()]
          , initStates = [()]
@@ -38,9 +36,31 @@ epsA = A { states = [()]
          , transition = curry $ const []
          }
 
+-- an automaton that recognizes one word
 symA :: Eq a => a -> Auto a Bool
 symA c = A { states = [True, False]
            , initStates = [False]
            , isAccepting = id
            , transition = (\st l -> if not st && l == c then [True] else [])
            }
+
+-- gives an automaton that recognizes the same
+-- language as <auto>, but which's type is
+-- Either q r, where q is the type of states
+-- of the former automaton
+leftA :: Auto a q -> Auto a (Either q r)
+leftA aut = A { states = map Left $ states aut
+              , initStates = map Left $ initStates aut
+              , isAccepting = either (isAccepting aut) (const False)
+              , transition = either (\st l -> map Left $ (transition aut st l)) (const $ const [])
+              }
+
+-- an automaton that recognizes languages
+-- that are recognized by both aut1 and aut2
+sumA :: Auto a q1 -> Auto a q2 -> Auto a (Either q1 q2)
+sumA aut1 aut2 = A { states = concat [(map Left $ states aut1) (map Right $ states aut2)]
+                   , initStates = concat
+                       [(map Left $ initStates aut1) (map Right $ initStates aut2)]
+                   , isAccepting = (map Left . isAccepting aut1) (map Right .isAccepting aut2)
+                   , transition = either (transition aut1) (transition aut2)
+                   }
