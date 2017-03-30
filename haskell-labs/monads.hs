c4726caecdc490f66ea70a448b39d89ded8ed10d
Filip Binkiewicz <filip.binkiewicz@gmail.com>
2017-03-30 13:10:47 +0200

    Monad tranformers

--- haskell-labs/monads.hs
+++ haskell-labs/monads.hs
@@ -1,5 +1,8 @@
 import Control.Applicative
 import Control.Monad
+import Control.Monad.Writer
+import Control.Monad.State
+import Data.Functor.Identity
 
 newtype This a = That a
 
@@ -54,3 +57,28 @@ eval (EDiv exp1 exp2) = do
     x1 <- eval exp1
     x2 <- eval exp2
     if (x2 == 0) then Err' "division by 0" else return (x1 `quot` x2)
+
+
+exists :: (Ord a) => a -> Tree a -> (Writer [a] Bool)
+exists _ Leaf = return False
+exists x (Node lt v rt) = do
+    tell [v]
+    if x == v then
+        return True
+    else
+        if x < v then
+            exists x lt
+        else
+            exists x rt
+
+exTree :: Tree Int
+exTree = Node (Node Leaf 1 Leaf) 3 (Node Leaf 5 Leaf)
+
+sumTree :: (Tree Int) -> ((WriterT String (StateT Int Identity)) ())
+sumTree Leaf = return ()
+sumTree (Node tl a tr) = do
+    sumTree tl
+    modify (+a)
+    sumTree tr
+
+runSum = runIdentity (runStateT (runWriterT (sumTree exTree)) 0)
